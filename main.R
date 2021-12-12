5dda62f30d12b983b6d624c6e89d47d1924e3484
Yahya <mohamedyahyamhmd@gmail.com>
2021-12-12 16:45:42 +0100

    I have added my files

---
+++ main.R
@@ -0,0 +1,12 @@
+library(reader)
+library(dplyr)
+
+appearances = read.csv("data/appearances.csv")
+characters = read.csv("data/characters.csv")
+episodes = read.csv("data/episodes.csv")
+scenes = read.csv("data/scenes.csv")
+
+seine_maritime = read.csv("data/data_seine_maritime.csv")
+
+
+killers = characters %>% select(name, killedBy) %>% na.omit()
