3574cbbeedc7548ce5bc487030dc4a9403e676ec
Meyss <medvihelbarka@gmail.com>
2021-12-13 21:13:05 +0100

    Last version

--- Tp_got.R
+++ Tp_got.R
@@ -10,94 +10,157 @@ episodes = read_csv('data/episodes.csv')
 populations = read_csv('data/populations.csv')
 locations=st_read("./data/GoTRelease/Locations.shp",crs=4326)
 
+#Analyse globale de la série
 
-#nbdeath par sex
-char_sex = characters %>% 
+#La mort par sex
+
+#Créer une dataframe des morts par sex
+deaths_by_sex = characters %>% 
   select(sex, killedBy) %>% 
   group_by(sex)%>% 
-  summarize(death_per_sex=n())%>%
+  summarize(nbd_by_sex=n())%>%
   na.omit()
-ggplot(char_sex, aes(x="", y=death_per_sex, fill=sex)) +
+#Construire le piechart montrant le nombre de morts par sex
+ggplot(deaths_by_sex, aes(x="", y=nbd_by_sex, fill=sex)) +
   geom_bar(stat="identity", width=1) +
   coord_polar("y", start=0)
 
-#nbdeath par house
+
+#La mort par house
+
+#Créer une dataframe des morts par house
 
 char_house = characters %>% 
   select(house, killedBy) %>% 
   na.omit() %>% group_by(house) %>% 
-  summarise(nb=n()) 
-char_house_ordered = char_house %>% 
-  arrange(desc(nb)) %>% 
-  mutate(house=factor(house,levels=house))
-ggplot(data = char_house_ordered)+ 
-  geom_bar(aes(x=house, y=nb), stat='identity')+ 
+  summarise(nbd_by_house=n())%>%
+  arrange(desc(nbd_by_house)) %>% 
+  mutate(house=factor(house,levels=house)) #ordonner le dataframe par nombre de morts cummulé par house 
+#Construire le barplot mobtrant la mort par house
+ggplot(data = char_house)+ 
+  geom_bar(aes(x=house, y=nbd_by_house), stat='identity')+ 
   scale_x_discrete("house")+ 
   scale_y_continuous("deaths")+ coord_flip()
 
 
-#nbdeath par saison 
+#La mort par saison 
 
-death_episode = episodes %>% 
+#Créer un dataframe de nombre de mort par episode dans un saison
+death_by_episode = episodes %>% 
   left_join(scenes) %>% 
   group_by(seasonNum, episodeNum) %>% 
-  summarize (nbe=sum(nbdeath))
-death_saison = death_episode %>% 
+  summarize (nbd_by_episode=sum(nbdeath))
+#Créer un dataframe de nombre de mort par par saison
+death_by_season = death_by_episode %>% 
   group_by(seasonNum) %>% 
-  summarize (nbs=sum(nbe)) 
-seasons = death_saison %>% 
-  arrange(nbs) %>% 
-  mutate(snb=factor(seasonNum,levels = seasonNum))
-df = death_episode %>% left_join(seasons) %>% filter(!is.na(snb))
-ggplot(data = df)+ 
-  geom_bar(aes(y=snb,x=nbe,fill=factor(episodeNum,level=10:1)), stat="identity")+ 
-  scale_fill_brewer("episode",palette = "Spectral")+theme_bw()+ 
-  geom_text(data=seasons,aes(y=snb,x=nbs, label=paste(round(nbs),'deaths')),hjust = "left")+ 
+  summarize (nbd_by_season=sum(nbd_by_episode))%>% 
+  arrange(nbd_by_season) %>% 
+  mutate(nbs=factor(seasonNum,levels = seasonNum))
+#Créer un dataframe de nombre de mort par par saison et par episode
+death_by_epi_seas= death_by_episode %>% left_join(death_by_season) %>% filter(!is.na(nbs))
+ggplot(data = death_by_epi_seas)+ 
+  geom_bar(aes(y=nbs,x=nbd_by_episode,fill=factor(episodeNum,level=10:1)), stat="identity")+ 
+  scale_fill_brewer("episode",palette = "RdYlGn")+theme_bw()+ 
+  geom_text(data=death_by_season,aes(y=nbs,x=nbd_by_season, label=paste(round(nbd_by_season))),hjust = "left")+ 
   scale_x_continuous("deaths", breaks = seq(0,100,by=25),limits = c(0,100),expand = c(0,1))+ 
   scale_y_discrete("season")
 
 
-# Death evolution per season
-deaths = scenes %>% 
+# Evolution de la mort durant la série
+
+#Créer un dataframe des morts dans la série par saison
+deaths_in_serie = scenes %>% 
   left_join(episodes) %>% 
   group_by(seasonNum) %>% 
-  summarize(nbd=sum(nbdeath))
-ggplot(data=deaths)+
-  geom_line(aes(x=seasonNum, y=nbd), stat='identity')+
-  scale_x_continuous("season", breaks =seq(1,8, by=1), limits =c(1,8), expand=c(0,1))+
-  scale_y_continuous("deaths")
+  summarize(nbd_in_serie=sum(nbdeath))
+#Construire le lineplot montrant l'évolution de la mort dans la série
+ggplot(data=deaths_in_serie)+
+  geom_line(aes(x=seasonNum, y=nbd_in_serie), stat='identity')+
+  scale_x_continuous("Season", breaks =seq(1,8, by=1), limits =c(1,8), expand=c(0,1))+
+  scale_y_continuous("Deaths")
 
 
-#Death par episode
-death_episode=scenes%>%
-  left_join(episodes) %>%filter(seasonNum==1)%>% 
-  group_by(episodeTitle, episodeNum) %>% 
-  summarize(nbe=sum(nbdeath), duration_epi=sum(duration))
-ggplot(death_episode,aes(x=duration_epi/60,y=nbe,col=factor(episodeNum)))+
-  geom_point(aes(size=nbe))+
-  geom_text(data=death_episode,aes(label=episodeTitle),vjust=-0.6)+
-  scale_x_continuous("Durée de l'épisode",seq (50, 65, by=3), limits = c(50,65))+
-  scale_y_continuous("Nombre des morts",limits = c(0,8))+
-  scale_color_brewer("Episode",palette ="Spectral")+
-  guides(colour = "legend", size = "legend")+
-  theme_bw()
+#Analuse par saison
 
-#Death per appearance
-death_char =  scenes%>% 
+#La mort par episode dans chaque saison
+
+#Créer un dataframe de morts par saison et leurs durée d'apparition par episode
+death_by_episode = episodes %>% 
+  left_join(scenes) %>% 
+  group_by(seasonNum, episodeNum) %>% 
+  summarize (nbd_by_episode=sum(nbdeath))
+#Créer un dataframe de nombre de mort par par saison
+death_by_season = death_by_episode %>% 
+  group_by(seasonNum) %>% 
+  summarize (nbd_by_season=sum(nbd_by_episode))%>% 
+  arrange(nbd_by_season) %>% 
+  mutate(nbs=factor(seasonNum,levels = seasonNum))
+#Créer un dataframe de nombre de mort par par saison et par episode
+death_by_epi_seas= death_by_episode %>% left_join(death_by_season) %>% filter(!is.na(nbs))
+#Construire le scatterplot montrant le nombre des morts par épisode dans chaque saison
+ggplot(data = death_by_epi_seas)+ 
+  geom_bar(aes(y=nbs,x=nbd_by_episode,fill=factor(episodeNum,level=10:1)), stat="identity")+ 
+  scale_fill_brewer("episode",palette = "RdYlGn")+theme_bw()+ 
+  geom_text(data=death_by_season,aes(y=nbs,x=nbd_by_season, label=paste(round(nbd_by_season))),hjust = "left")+ 
+  scale_x_continuous("deaths", breaks = seq(0,100,by=25),limits = c(0,100),expand = c(0,1))+ 
+  scale_y_discrete("season")
+
+
+#La mort par apparence
+
+#Créer un dataframe de nombre de mort par episode et leurs durées d'apparition
+death_epi_by_app =  scenes%>% 
   left_join(appearances) %>%
   left_join(characters) %>%filter(!is.na(killedBy)) %>% 
   left_join(episodes) %>%filter(seasonNum=="1") %>% 
   group_by(seasonNum, episodeNum, name)%>% 
-  summarize(dur_Car=sum(duration)) %>% 
-  arrange(desc(dur_Car))
-char = death_char %>% group_by(seasonNum, name)%>%
-  summarize(dur_seas=sum(dur_Car))%>%
+  summarize(dur_epi=sum(duration)) %>% 
+  arrange(desc(dur_epi))
+#Créer un dataframe de nombre de mort par saison et leurs durées d'apparition
+death_seas_by_app = death_epi_by_app %>% group_by(seasonNum, name)%>%
+  summarize(dur_seas=sum(dur_epi))%>%
   filter(dur_seas>60*45) %>% 
   arrange(dur_seas)%>%
   mutate(nameC=factor(name,levels = name))
-db = death_char %>% left_join(char) %>%filter(!is.na(nameC))
+#Créer un dataframe de nombre de mort par par saison et par episode et leur durée d'apparition
+db = death_epi_by_app %>% left_join(death_seas_by_app) %>%filter(!is.na(nameC))
+#Construire le boxplot montrant la durée d'apparition des personnages mortes dans chaque saison
 ggplot(data=db)+
-  geom_boxplot(aes(x=nameC,y=dur_Car, fill=factor(seasonNum)))+
-  scale_x_discrete("Characters Mortes")
+  geom_boxplot(aes(x=nameC,y=dur_epi, fill=factor(seasonNum)))+
+  scale_x_discrete("Characters Mortes")+
+  scale_y_continuous("durée d'apparition")
 
 
+#Les killers et les familles qu'il a tuée le plus 
+
+#Créer un dataframe des plus grands meurterieux de la série
+top_killers = characters %>% 
+  group_by(killedBy) %>% filter(!is.na(killedBy)) %>% 
+  summarise(nb =n()) %>% 
+  arrange(desc(nb)) %>% 
+  head(5)
+#Créer un dataframe des houses le plus touchés par la mort
+top_houses_killed=characters %>% filter(!is.na(killedBy)) %>%filter(!is.na(house))%>%
+  group_by(house) %>% 
+  summarize(nbk=n()) %>% arrange(desc(nbk)) %>% head(5)
+#Créer un dataframe des meurterieux et des houses le plus touchés par la mort
+killers=top_killers %>% 
+  left_join(characters) %>% filter(!is.na(killedBy)) %>% filter(!is.na(house))%>%
+  left_join(top_houses_killed) %>% 
+  group_by (killedBy, house)  %>%
+  summarize(nbk=n())
+killers=pivot_wider(killers,names_from=house,values_from=nbk)
+killers[is.na(killers)]=0
+killers=killers%>% select(Stark,Lannister,Dothraki,`Gard Royal`,'Night Gard')
+killers=killers %>% filter(killedBy=='Jon Snow')
+dt=subset(killers, select = -c(killedBy))
+colnames(dt) <- c('Stark','Lannister','Dothraki','`Gard Royal`','Night Gard')
+rownames(dt) <- c('Jon Snow')
+dt = rbind(rep(7,5) , rep(0,5) , dt)
+colors_border=c( rgb(0.2,0.5,0.5,0.5), rgb(0.8,0.2,0.5,0.5) , rgb(0.7,0.5,0.1,0.5), rgb(1,0.7,0.8,0.5), rgb(0.5,0,0.5,0.5) )
+colors_in=c( rgb(0.2,0.5,0.5,0.4), rgb(0.8,0.2,0.5,0.5) , rgb(0.7,0.5,0.1,0.5), rgb(1,0.7,0.8,0.5), rgb(0.5,0,0.5,0.5) )
+radarchart( dt  , axistype=1 , 
+            pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
+            cglcol="blue", cglty=1, axislabcol="blue", caxislabels=seq(0,8,2), cglwd=0.8,
+            vlcex=0.8)
+legend(x=1.6, y=1, legend = 'Jon Snow', bty = "n", pch=20 , col=colors_in , text.col = "grey", cex=1.2, pt.cex=5)
